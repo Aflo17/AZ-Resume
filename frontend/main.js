77efc7d22f1417a35f07da6d2c2e39f58817f749
Adonis Flores <aaflores6@gmail.com>
2025-01-08 22:27:13 -0500

    Deployed Storage and function, and removed a duplicate fetch request that unintentional.

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,8 @@ if (!window.counterInitialized) {
     window.counterInitialized = true; // Prevent duplication
 
     document.addEventListener('DOMContentLoaded', () => {
-        const apiUrl = "http://localhost:7071/api/GetResumeCounter";
+        const apiUrl = "https://getresumecounter17.azurewebsites.net/api/GetResumeCounter?code=-7yqiVU2EbkDHmfOYw_JqtlUxcYTIwq3AkuW7tLDyGf9AzFu7Xgmug%3D%3D";
+        const Localapi = "http://localhost:7071/api/GetResumeCounter"; //this is for local testing
 
         fetch(apiUrl)
             .then(response => response.json())
